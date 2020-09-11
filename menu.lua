614536086e7e86684c29c9a321077e03e284f63d
dasjo02 <pikturemerollin@gmail.com>
2020-09-11 14:46:08 -0400

    Add files via upload

--- menu.lua
+++ menu.lua
@@ -1,10 +1,36 @@
-print("+--------------------------------------+");
-print("| Welcome "..os.date().."     |");
-print("+---------------------------------------");
-print("| 1. Generate random enemy position    |");
-print("| 2. Distance from enemy to player     |");
-print("| 3. Get angle from enemy to player    |");
-print("| 4. Exit                              |");
-print("+--------------------------------------+");
+math.randomseed(os.time());
 
-print("\nPlease select an option: ");
+local player_x, player_y = 400, 300;
+local enemy_x, enemy_y = 0,0;
+local user_option = 0;
+
+while user_option ~= 4 do
+    print("+--------------------------------------+");
+    print("| Welcome "..os.date().."     |");
+    print("+---------------------------------------");
+    print("| 1. Generate random enemy position    |");
+    print("| 2. Distance from enemy to player     |");
+    print("| 3. Get angle from enemy to player    |");
+    print("| 4. Exit                              |");
+    print("+--------------------------------------+");
+
+    print("\nPlease select an option: ");
+    user_option = io.read("*n");
+
+    if user_option == 1 then
+        enemy_x = math.random(0,800);
+        enemy_y = math.random(0,600);
+        print("New enemy position ("..enemy_x..","..enemy_y..")")
+    elseif user_option == 2 then
+        local d = math.sqrt((enemy_x-player_x)^2 + (enemy_y - player_y) ^2);
+        print("Distance from enemy to player: "..d)
+    elseif user_option == 3 then
+        print("Chose Option 3");
+    elseif user_option == 4 then
+        print("Chose Option 4");
+    else
+        print("You chose an invalid option, please try again");
+    end
+end
+
+print("Goodbye");
